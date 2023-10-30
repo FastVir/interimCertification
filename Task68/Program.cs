a5f0b983ac21d369adaa08349f46f5471271a681
Dimitry Timofeev <fasvir32gar@gmail.com>
2023-10-30 18:59:37 +0300

    added changes to the solution of tasks

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -4,12 +4,11 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-
-int Ack(int m, int n)
+int FuncAkkerman(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (n == 0) return Ack(m - 1, 1);
-    else return Ack(m - 1, Ack(m, n - 1));
+    else if (n == 0) return FuncAkkerman(m - 1, 1);
+    else return FuncAkkerman(m - 1, FuncAkkerman(m, n - 1));
 }
 
 int InputNumbers(string input)
@@ -22,5 +21,5 @@ int InputNumbers(string input)
 
 int m = InputNumbers("Введите M: ");
 int n = InputNumbers("Введите N: ");
-int functionAkkerman = Ack(m, n);
+int functionAkkerman = FuncAkkerman(m, n);
 Console.Write($"Функция Аккермана = {functionAkkerman} ");
